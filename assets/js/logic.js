d99155a1415b77146aeb6abc65f2a53786104ab0
dimshik100 <dimshik100@gmail.com>
2017-03-31 22:44:20 +0300

    Added choozy button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -11,30 +11,31 @@ function askUserToRemoveAds() {
     closeOnCancel: false,
     imageUrl: "images/choozy.png"
   },
-
     function (isConfirm) {
       if (isConfirm) {
+        removeAds();
         swal("Removed!", "All ads were removed for this session", "success");
+        $('.choozy-btn').hide('slow');
       } else {
         showAds();
-        showChoozyBtn();
+        $('.choozy-btn').show('slow');
         swal("Remember", "You can still remove the ads by clicking the Choozy button", "info");
       }
     });
 }
-
-
-
-function showChoozyBtn() {
-
-}
-
 
 function showAds() {
   $('.add_banner').html('<a href="#"><img src="images/addbanner_728x90_V1.jpg" alt=""></a>');
   $('.sideAdd').html('<img src="images/add_img.jpg" alt="">');
 }
 
+function removeAds() {
+  $('.add_banner').html('');
+  $('.sideAdd').html('');
+}
+
 
 askUserToRemoveAds();
+
+$('.choozy-btn').on('click',askUserToRemoveAds);
 
